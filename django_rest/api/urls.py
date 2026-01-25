262f09f8b5e11ef27393e1b24600be70da5e72ef
Shivansh Mahajan <shivansh.m2003@gmail.com>
2026-01-25 11:25:38 +0530

    class based view

--- django_rest/api/urls.py
+++ django_rest/api/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path('students/', views.studentView),
     path('students/<int:pk>/', views.studentDetailView),
+    path('employee/', views.employeeView.as_view()),
+    path('employee/<int:pk>/', views.employeeDetailView.as_view()),
 ]
